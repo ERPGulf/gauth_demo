c8119591a665d387dea138144b0fddebf1717162
Muhsina13 <muhsina@htsqatar.com>
2025-03-25 21:20:03 +0530

    swagger demo

--- src/pages/SwaggerPage.tsx
+++ src/pages/SwaggerPage.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
-import mySwaggerJson from "./swagger.json"; // Import the local JSON file
+import mySwaggerJson from "./swagger.json";
 
 const SwaggerPage: React.FC = () => {
   return (
-    <div style={{ width: "100%", height: "100vh", padding: "20px" }}>
+
+    <div style={{
+      width: "100%", height: "100vh", padding: "20px"
+    }}>
       <SwaggerUI spec={mySwaggerJson} />  {/* Load your API list */}
     </div>
   );
 };
-
 export default SwaggerPage;
